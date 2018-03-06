f502e5f5337f3a14b2b04379d14728c6fbb2befe
joker <joker_zzp@qq.com>
2018-03-06 18:09:28 +0800

    2018-03-06

--- code/command/linux/shell/check_command.sh
+++ code/command/linux/shell/check_command.sh
@@ -1,7 +1,22 @@
 #! /bin/bash
 
 var=${1}
+
 if command -v ${var} > /dev/null 2>&1;
+then
+    echo "命令存在"
+else
+    echo "命令不存在"
+fi
+
+if type ${var} >/dev/null 2>&1;
+then
+    echo "命令存在"
+else
+    echo "命令不存在"
+fi
+
+if hash ${var} 2>/dev/null;
 then
     echo "命令存在"
 else
